07fb9f8312e458678d511c28f32c6e2d6b96f64e
Frank Lai <Frank Lai@localhost>
2014-01-12 17:32:06 +0800

    add encrypted signature support
    not really fully support, but add one decrypt method

--- modules/fujirou_youtube/host.php
+++ modules/fujirou_youtube/host.php
@@ -26,12 +26,25 @@ class FujirouHostYouTube
         $response = new Curl($url);
         $html = $response->get_content();
 
+        // parse player url
+        $this->playerUrl = $this->getPlayerUrl($html);
+
         // 2. find url_encoded_fmt_stream_map
         $encodedMapString = $this->getMapString($html);
         $mapString = $encodedMapString;
 
+//         $adaptiveFormats = $this->getAdaptiveFmts($html);
+//         $adaptiveMap = $this->parseMapString($adaptiveFormats, $html);
+// echo "\n == encoded ==\n";
+// echo $mapString;
+// echo "\n == adaptive ==\n";
+// echo $adaptiveFormats;
+// echo "\n\n";
+// print_r($adaptiveMap);
+// echo "\n\n";
+
         // 3. parse map string
-        $videoMap = $this->parseMapString($mapString);
+        $videoMap = $this->parseMapString($mapString, $html);
         $video = $this->chooseVideo($videoMap);
         if (empty($video)) {
             return $ret;
@@ -72,7 +85,7 @@ class FujirouHostYouTube
         return sprintf("%s (%s)", $title, $itagString);
     }
 
-    private function parseMapString($mapString)
+    private function parseMapString($mapString, $html)
     {
         $urlList = explode(',', $mapString);
 
@@ -87,15 +100,111 @@ class FujirouHostYouTube
 
             parse_str($queries, $items);
 
-            $videoMap[$items['itag']] = $items['url'] . "&signature=" . $items['sig'];
+            $signature = '';
+            if (array_key_exists('sig', $items)) {
+                $signature = $items['sig'];
+            } elseif (array_key_exists('s', $items)) {
+                // encrypted signature
+                $encrypted = $items['s'];
+                $signature = $this->decryptSignature($encrypted);
+            }
+
+            if ($signature === '') {
+                printf("Failed to parse signature");
+            }
+
+            $videoMap[$items['itag']] = $items['url'] . "&signature=" . $signature;
         }
 
         return $videoMap;
     }
 
+    private function getPlayerUrl($html)
+    {
+        $pattern = '/"assets":.+?"js":\s*("[^"]+")/';
+        $jsonUrl = Common::getFirstMatch($html, $pattern);
+        $url = json_decode($jsonUrl, true);
+
+        return $url;
+    }
+
+    private function decryptSignature($encrypted)
+    {
+        // get player content first
+
+        $url = $this->playerUrl;
+        if (substr($url, 0, 2) === '//') {
+            $url = 'https:' . $url;
+        }
+
+        $response = new Curl($url);
+        $html = $response->get_content();
+
+        $pattern = '/signature=([a-zA-Z]+)/';
+        $funcName = Common::getFirstMatch($html, $pattern);
+
+        $pattern = sprintf("/function %s\(.*?\)\{(.+?)\}/", $funcName);
+        $funcContent = Common::getFirstMatch($html, $pattern);
+
+
+        $pattern = sprintf("/function %s\(.*?\)\{(.+?)\}/", 'Sk');
+        $Sk = Common::getFirstMatch($html, $pattern);
+
+//         if ($playerId === 'vfln8xPyM')
+        // mane shite
+
+        $a = str_split($encrypted);
+
+        // swap(0, 36)
+        // swap(0, 14)
+        // slice(1)
+        // reverse()
+        // slice(1)
+        // swap(0, 54)
+        $a = $this->swap($a, 0, 36);
+        $a = $this->swap($a, 0, 14);
+        $a = array_slice($a, 1);
+        $a = array_reverse($a);
+        $a = array_slice($a, 1);
+        $a = $this->swap($a, 0, 54);
+
+        $decrypted = implode('', $a);
+
+//         echo "\n== == ==\n";
+//         echo "player url: $url";
+//         echo "\n== == ==\n";
+//         echo "funcName: $funcName";
+//         echo "\n== == ==\n";
+//         echo "content: $funcContent";
+//         echo "\n== == ==\n";
+//         echo "Sk: $Sk";
+//         echo "\n== == ==\n";
+//         echo "encrypted: $encrypted";
+//         echo "\n== == ==\n";
+//         echo "decrypted: $decrypted";
+//         echo "\n== == ==\n";
+//         echo "\n== == ==\n";
+        return $decrypted;
+    }
+
+    private function swap(&$array, $x, $y)
+    {
+        $tmp = $array[$x];
+        $array[$x] = $array[$y];
+        $array[$y] = $tmp;
+
+        return $array;
+    }
+
     private function getMapString($html)
     {
         $pattern = '/"url_encoded_fmt_stream_map": "([^"]+)"/';
+        return Common::getFirstMatch($html, $pattern);
+    }
+
+    private function getAdaptiveFmts($html)
+    {
+        $pattern = '/"adaptive_fmts": "([^"]+)"/';
         return Common::getFirstMatch($html, $pattern);
     }
 
@@ -112,9 +221,9 @@ class FujirouHostYouTube
         $itagMap = array(
             "37" => "mp4", // "1080p, MP4, H.264, AAC"
             "22" => "mp4", // "720p, MP4, H.264, AAC",
+            "34" => "flv", // "360p, FLV, H.264, AAC",
             "18" => "mp4", // "360p, MP4, H.264, AAC",
             "35" => "flv", // "480p, FLV, H.264, AAC",
-            "34" => "flv", // "360p, FLV, H.264, AAC",
             "5"  => "flv" // "240p, FLV, H.263, MP3",
         );
 
@@ -143,14 +252,27 @@ if (!empty($argv) && basename($argv[0]) === basename(__FILE__)) {
     $module = 'FujirouHostYouTube';
 //    $url = 'http://www.youtube.com/watch?v=tNC9V2ewsb4';
 //     $url = 'https://www.youtube.com/watch?v=-jej8YS4Slk';
-    $url = 'http://www.youtube.com/watch?v=DzOQ-shPKV4';
+//    $url = 'http://www.youtube.com/watch?v=RY35O02Fg8M';
+    $url = 'http://www.youtube.com/watch?v=iul4SBlHIf8';
+$url = 'http://www.youtube.com/watch?v=FXg4LXsg14s';
+$url = 'http://www.youtube.com/watch?v=tNo3LuZXA1w';
+$url = 'http://www.youtube.com/watch?v=Ci8REzfzMHY';
+$url = 'http://www.youtube.com/watch?v=UHFAjkD_LLg'; // Taylor Swift feat Paula Fernandes Long Live VEVO 1080p
+
+    if ($argc >= 2) {
+        $argument = $argv[1];
+        if (substr(strtolower($argument), 0, 4) === 'http') {
+            $url = $argument;
+        }
+    }
 
     $refClass = new ReflectionClass($module);
     $obj = $refClass->newInstance($url, '', '', array());
 
+    echo "Get download info of '$url'\n\n";
     $info = $obj->GetDownloadInfo();
 
-    var_dump($info);
+    print_r($info);
 }
 
 // vim: expandtab ts=4
