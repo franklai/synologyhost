d82e210d549ec114e3680bd485bc8cae478110fa
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2014-11-08 18:36:09 +0800

    fix dailymotion and bump to 1.3

--- modules/dailymotion/host.php
+++ modules/dailymotion/host.php
@@ -55,6 +55,7 @@ class FujirouHostDailymotion
 
         $json = $this->getJsonFromHtml($html);
         if (!$json) {
+            Common::debug('Failed to get json from html.');
             return $ret;
         }
 
@@ -83,9 +84,10 @@ class FujirouHostDailymotion
     private function getJsonFromHtml($html)
     {
         $prefix = 'var info = {';
-        $suffix = '}},';
+        $suffix = "}},\n";
         $js = Common::getSubString($html, $prefix, $suffix);
         if (empty($js)) {
+            Common::debug('Failed to find info json');
             return false;
         }
 
@@ -130,6 +132,7 @@ if (basename($argv[0]) === basename(__FILE__)) {
 //    $url = 'http://www.dailymotion.com/video/x1u5p24_20080809-vs';
     $url = 'http://www.dailymotion.com/video/x1dt64a_%E5%AE%89%E5%AE%A4%E5%A5%88%E7%BE%8E%E6%81%B5-contrail-500th-live-%E3%82%A2%E3%83%B3%E3%82%B3%E3%83%BC%E3%83%AB_music';
     $url = 'http://www.dailymotion.com/video/x1xnacg_2014-05-30-%E5%B5%90-%E8%AA%B0%E3%82%82%E7%9F%A5%E3%82%89%E3%81%AA%E3%81%84-m%E3%82%B9%E3%83%86_music';
+    $url = 'http://www.dailymotion.com/video/x26l0ov_aa';
 
     if (count($argv) >= 2 && 0 === strncmp($argv[1], 'http://', 7)) {
         $url = $argv[1];
