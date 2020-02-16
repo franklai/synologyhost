55be79be7d8fdc478133c178f64126c578e83818
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2020-02-16 15:09:14 +0800

    fix daily motion parsing

--- modules/dailymotion/host.php
+++ modules/dailymotion/host.php
@@ -1,15 +1,16 @@
 <?php
 
 if (!class_exists('Common')) {
-    require('common.php');
+    require 'common.php';
 }
 if (!class_exists('Curl')) {
-    require('curl.php');
+    require 'curl.php';
 }
 
 class FujirouHostDailymotion
 {
-    public function __construct($url, $username, $password, $hostInfo) {
+    public function __construct($url, $username, $password, $hostInfo)
+    {
         $this->url = $url;
         $this->username = $username;
         $this->password = $password;
@@ -22,11 +23,12 @@ class FujirouHostDailymotion
             'stream_h264_hd_url',
             'stream_h264_hq_url',
             'stream_h264_url',
-            'stream_h264_ld_url'
+            'stream_h264_ld_url',
         );
     }
 
-    public function onDownloaded() {
+    public function onDownloaded()
+    {
         // dummy function to avoid PHP error log
         return true;
     }
@@ -39,9 +41,10 @@ class FujirouHostDailymotion
     // other optional key is
     // "filename"
     // "cookiepath"
-    public function GetDownloadInfo() {
+    public function GetDownloadInfo()
+    {
         $ret = array(
-            DOWNLOAD_ERROR => 'ERR_FILE_NO_EXIST'
+            DOWNLOAD_ERROR => 'ERR_FILE_NO_EXIST',
         );
 
         $url = $this->url;
@@ -61,7 +64,7 @@ class FujirouHostDailymotion
         );
         Common::debug("JSON url is [$jsonUrl]");
 
-        $response = new Curl($jsonUrl, NULL, NULL, NULL);
+        $response = new Curl($jsonUrl, null, null, null);
         $html = $response->get_content();
 
         $json = $this->getJsonFromHtml($html);
@@ -84,9 +87,9 @@ class FujirouHostDailymotion
         $filename = Common::sanitizePath($videoTitle) . ".mp4";
 
         $ret = array(
-            DOWNLOAD_URL      => $videoUrl,
+            DOWNLOAD_URL => $videoUrl,
             DOWNLOAD_FILENAME => $filename,
-            DOWNLOAD_COOKIE   => $this->tmpCookiePath
+            DOWNLOAD_COOKIE => $this->tmpCookiePath,
         );
 
         return $ret;
@@ -95,14 +98,14 @@ class FujirouHostDailymotion
     private function getJsonFromHtml($html)
     {
         $prefix = '{"context":';
-        $suffix = "}};\n";
+        $suffix = "};\n";
         $js = Common::getSubString($html, $prefix, $suffix);
         if (empty($js)) {
             Common::debug('Failed to find info json');
             return false;
         }
 
-        $pattern = '/(\{"context":.*\}\});/';
+        $pattern = '/(\{"context":.*\});/';
         $json_string = Common::getFirstMatch($js, $pattern);
         return json_decode($json_string, true);
     }
@@ -115,6 +118,12 @@ class FujirouHostDailymotion
             return $id;
         }
 
+        $pattern = '/dailymotion.com\/embed\/video\/([a-zA-Z0-9]+)/i';
+        $id = Common::getFirstMatch($url, $pattern);
+        if ($id) {
+            return $id;
+        }
+
         // try playlist type
         $pattern = '/dailymotion.com\/playlist\/.*#video=(.*)/i';
         $id = Common::getFirstMatch($url, $pattern);
@@ -122,7 +131,8 @@ class FujirouHostDailymotion
         return $id;
     }
 
-    private function getVideoUrl($json) {
+    private function getVideoUrl($json)
+    {
         $url = null;
 
         if (!array_key_exists('metadata', $json)) {
@@ -137,7 +147,7 @@ class FujirouHostDailymotion
         $qualities = $metadata['qualities'];
 
         foreach ($qualities as $q => $items) {
-            Common::debug("quality [".$q."]");
+            Common::debug("quality [" . $q . "]");
 
             foreach ($items as $item) {
                 $type = $item['type'];
@@ -150,7 +160,7 @@ class FujirouHostDailymotion
 
         $hash = md5($url);
         $this->tmpCookiePath = "/tmp/dailymotion.cookie.$hash.txt";
-        $response = new Curl($url, NULL, NULL, $this->tmpCookiePath);
+        $response = new Curl($url, null, null, $this->tmpCookiePath);
         $location = $response->get_header('Location');
         if (empty($location)) {
             return $url;
@@ -165,7 +175,8 @@ class FujirouHostDailymotion
         return substr($location, 0, $pos);
     }
 
-    private function getVideoTitle($json) {
+    private function getVideoTitle($json)
+    {
         if (!array_key_exists('metadata', $json)) {
             return null;
         }
@@ -182,14 +193,14 @@ class FujirouHostDailymotion
 if (!empty($argv) && basename($argv[0]) === basename(__FILE__)) {
     $module = 'FujirouHostDailymotion';
 //     $url = 'http://www.dailymotion.com/video/xtdg4e_2012-mtv-vma-performance-recap-pink-taylor-swift-lil-wayne_music';
-//     $url = 'http://www.dailymotion.com/video/xnqusv_the-next-list-jake-shimabukuro_lifestyle?search_algo=2';
-//     $url = 'http://www.dailymotion.com/video/x25ef_jake-shimabukuro-virtuose-du-ukulel_music?search_algo=2';
-//     $url = 'http://www.dailymotion.com/video/xa02_while-my-ukulele-gently-weeps_music';
-//    $url = 'http://www.dailymotion.com/video/x1u5p24_20080809-vs';
+    //     $url = 'http://www.dailymotion.com/video/xnqusv_the-next-list-jake-shimabukuro_lifestyle?search_algo=2';
+    //     $url = 'http://www.dailymotion.com/video/x25ef_jake-shimabukuro-virtuose-du-ukulel_music?search_algo=2';
+    //     $url = 'http://www.dailymotion.com/video/xa02_while-my-ukulele-gently-weeps_music';
+    //    $url = 'http://www.dailymotion.com/video/x1u5p24_20080809-vs';
     $url = 'http://www.dailymotion.com/video/x2kmre5_56kast-52-on-a-tous-des-choses-a-cacher-et-des-points-a-relier_tech';
     $url = 'http://www.dailymotion.com/video/x2bzn2n_taylor-swift-blank-space-live-at-kiis-fm-jingle-ball-2014_music';
     $url = 'http://www.dailymotion.com/playlist/x1hlho_ginji030_perfume-2/1#video=xt1mw1';
-    
+
     $url = 'http://www.dailymotion.com/video/xn30yp_fairy-tail-bande-annonce-preview-film-2012_shortfilms'; // short, 00:31, 1.72MB
     $url = 'http://www.dailymotion.com/video/x4xeb5l_the-amazing-world-of-gumball-the-choices-s5e6_tv';
     $url = 'https://www.dailymotion.com/video/k5fGL5hXWOukIqsUUjz';
@@ -207,6 +218,4 @@ if (!empty($argv) && basename($argv[0]) === basename(__FILE__)) {
     var_dump($info);
 }
 
-    
 // vim: expandtab ts=4
-?>
